c92b97daf926358c586c6436557da5fe785bc2c4
Yannick boogaerts <yboogaerts@gmail.com>
2023-04-04 16:07:19 +0200

    beau format de date

--- src/be/technifutur/decouverte/date/TestDateTimeFormatter.java
+++ src/be/technifutur/decouverte/date/TestDateTimeFormatter.java
@@ -8,11 +8,12 @@ public class TestDateTimeFormatter {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy");
-        String text = date.format(formatter);
+        DateTimeFormatter writerFormatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy");
+        DateTimeFormatter readerFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
+        String text = date.format(writerFormatter);
         System.out.println(text);
         text = "21/1/2000";
-        LocalDate parsedDate = LocalDate.parse(text, formatter);
-        System.out.println(parsedDate);
+        LocalDate parsedDate = LocalDate.parse(text, readerFormatter);
+        System.out.println(parsedDate.format(writerFormatter));
     }
 }
